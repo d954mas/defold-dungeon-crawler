61ad41c473dadf2084e638552610dacae71b4741
d954mas <d954mas@mail.ru>
2018-09-27 01:21:33 +0300

    add fov map

--- scenes/game/gui/debug_gui.go
+++ scenes/game/gui/debug_gui.go
@@ -28,3 +28,23 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "debug_fov_map_sprite"
+  type: "sprite"
+  data: "tile_set: \"/scenes/game/gui/debug_fov_map.atlas\"\n"
+  "default_animation: \"empty\"\n"
+  "material: \"/resizer/assets/sprite_nearest.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 1398.0
+    y: 936.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
